8eb162bba5a4dfa8e9ed83bd99c4fd2472c34d0b
ulanbekovs <aktan.ulanbekov@gmail.com>
2022-02-09 23:13:41 +0600

    изменено

--- lesson-16.py
+++ lesson-16.py
@@ -108,7 +108,3 @@
 # print(s)
 
 
-
-name = ("всем привет")
-print(name)
-
--- lesson-17.py
+++ lesson-17.py
@@ -1,5 +1,13 @@
-#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
-
-#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
-
-print("hi everyone")
+def get_age(*args, **kwargs):
+    gte_30 = []
+    lte_30 = []
+    if kwargs:
+        for i in kwargs.values():
+            if i >= 30:
+                gte_30.append(i)
+            else:
+                lte_30.append(i)
+    gte_30 = list(set(gte_30))
+    lte_30 = list(set(lte_30))
+    return f'gte_30 : {gte_30} lte_30 : {lte_30}'
+print(get_age(nazi=32, nodira=18, samat=29))
